d70139a64963aa33bc81768ba246ac23e6c2aa5c
pokeymaster <cyd1995@hotmail.com>
2023-05-29 16:31:09 +1000

    deploy site

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-
-import App from './App';
-import './index.css';
-const container = document.getElementById('root');
-const roots = createRoot(container); // createRoot(container!) if you use TypeScript
-roots.render(<App tab="home" />);
